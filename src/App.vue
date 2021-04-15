09c5e8ef4913a19fa861c2ebd7b300d909705265
nitslaszlo <nitslaszlo@gmail.com>
2021-04-15 21:23:46 +0200

    UP

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <v-app>
     <!-- <v-navigation-drawer app> -->
-      <!-- -->
+    <!-- -->
     <!-- </v-navigation-drawer> -->
 
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img class="shrink mr-2" contain src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" />
-        <v-btn color="info" rounded class="links" to="/">Home</v-btn>
       </div>
-      <v-spacer></v-spacer>
-      <v-btn color="info" rounded class="links" to="/about">About</v-btn>
+      <v-btn color="transparent" elevation="0" to="/">Home</v-btn>
 
       <v-spacer></v-spacer>
 
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn color="transparent" elevation="0" to="/about">About</v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="https://github.com/nitslaszlo/JedlikComplexVuetify" target="_blank" text>
+        <span class="mr-2">GitHub</span>
+        <v-icon>mdi-github</v-icon>
       </v-btn>
     </v-app-bar>
 
