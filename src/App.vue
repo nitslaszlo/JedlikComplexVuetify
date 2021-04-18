3d162832f6587a4480a4668a6fc3d53cd3266c84
nitslaszlo <nitslaszlo@gmail.com>
2021-04-18 18:08:07 +0200

    up

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,75 @@
 <template>
-  <v-app>
-    <!-- <v-navigation-drawer app> -->
-    <!-- -->
-    <!-- </v-navigation-drawer> -->
+  <div id="app">
+    <v-app>
+      <v-app-bar app color="primary" dark absolute class="hidden-xs-only">
+        <div class="d-flex align-center">
+          <v-img src="@/assets/Jedlik.png" transition="scale-transition" width="30" />
+        </div>
 
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img class="shrink mr-2" contain src="@/assets/Jedlik.png" transition="scale-transition" width="40" />
-      </div>
+        <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
+        <v-btn color="transparent" elevation="0" to="/">Home</v-btn>
 
-      <v-btn color="transparent" elevation="0" to="/">Home</v-btn>
+        <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
+        <v-btn color="transparent" elevation="0" to="/demo">Demo</v-btn>
 
-      <v-btn color="transparent" elevation="0" to="/demo">Demo</v-btn>
+        <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
+        <v-btn color="transparent" elevation="0" to="/about">About</v-btn>
 
-      <v-btn color="transparent" elevation="0" to="/about">About</v-btn>
+        <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
+        <v-btn href="https://github.com/nitslaszlo/JedlikComplexVuetify" target="_blank" text>
+          <span class="mr-2">GitHub</span>
+          <v-icon>mdi-github</v-icon>
+        </v-btn>
+      </v-app-bar>
 
-      <v-btn href="https://github.com/nitslaszlo/JedlikComplexVuetify" target="_blank" text>
-        <span class="mr-2">GitHub</span>
-        <v-icon>mdi-github</v-icon>
-      </v-btn>
-    </v-app-bar>
+      <v-app-bar app color="primary" dark absolute class="hidden-sm-and-up">
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      </v-app-bar>
 
-    <v-main>
-      <v-container fluid>
-        <!-- If using vue-router -->
-        <router-view></router-view>
-      </v-container>
-    </v-main>
+      <v-navigation-drawer v-model="drawer" absolute top temporary>
+        <v-btn color="transparent" elevation="0" to="/">Home</v-btn>
 
-    <v-footer absolute fixed padless>
-      <v-card flat tile width="100%" class="primary lighten-1 text-center">
-        <v-card-text class="white--text">© {{ new Date().getFullYear() }} — <strong>Jedlik</strong> </v-card-text>
-      </v-card>
-    </v-footer>
-  </v-app>
+        <v-spacer></v-spacer>
+
+        <v-btn color="transparent" elevation="0" to="/demo">Demo</v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn color="transparent" elevation="0" to="/about">About</v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn href="https://github.com/nitslaszlo/JedlikComplexVuetify" target="_blank" text>
+          <span class="mr-2">GitHub</span>
+          <v-icon>mdi-github</v-icon>
+        </v-btn>
+      </v-navigation-drawer>
+
+      <v-main>
+        <v-container fluid>
+          <!-- If using vue-router -->
+          <router-view></router-view>
+        </v-container>
+      </v-main>
+
+      <v-footer absolute fixed padless>
+        <v-card flat tile width="100%" class="primary lighten-1 text-center">
+          <v-card-text class="white--text">© {{ new Date().getFullYear() }} — <strong>Jedlik</strong> </v-card-text>
+        </v-card>
+      </v-footer>
+    </v-app>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  private drawer = false;
+}
 </script>
